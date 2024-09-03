fe005a4c151c33f32c779fae49934bf280baa1c8
Koushik Choudhury <koushik.choudhury@philips.com>
2024-09-03 20:10:31 +0530

    New Color files added

---
+++ ColorEnums.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace TelCoColorCoder
+{
+    enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+    enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+
+    const char* MinorColorNames[] = {
+      "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+
+    const int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+}
